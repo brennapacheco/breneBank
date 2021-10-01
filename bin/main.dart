2864f52178b3be106b3b6a8238ae115cbc2f21d4
Brenna <brennapacheco@hotmail.com>
2021-10-01 13:28:45 -0300

    refatorando os métodos de saque e transferência + criação do método verificaSaldo

--- bin/main.dart
+++ bin/main.dart
@@ -4,16 +4,14 @@ void main() {
   contaDaBrenna.titular = "Brenna";
   contaDoCainaa.titular = "Cainãa";
 
-  contaDaBrenna.deposito(20.0);
-  print('Saldo da ${contaDaBrenna.titular}: R\$ ${contaDaBrenna.saldo}');
-  contaDaBrenna.saque(80.0); //acessando o método saque da classe ContaCorrente
-  print('Saldo da ${contaDaBrenna.titular}: R\$ ${contaDaBrenna.saldo}');
-
-  contaDoCainaa.deposito(50.0);
-  print('Saldo da ${contaDoCainaa.titular}: R\$ ${contaDoCainaa.saldo}');
-  contaDoCainaa.saque(130.0); //acessando o método saque da classe ContaCorrente
-  print('Saldo da ${contaDoCainaa.titular}: R\$ ${contaDoCainaa.saldo}');
-
+  print("Titular: ${contaDaBrenna.titular}, saldo: R\$ ${contaDaBrenna.saldo}.");
+  print("Titular: ${contaDoCainaa.titular}, saldo: R\$ ${contaDoCainaa.saldo}.");
+  
+  contaDaBrenna.transferencia(50.0, contaDoCainaa);
+  contaDaBrenna.deposito(75.0);
+  
+  print("Titular: ${contaDaBrenna.titular}, saldo: R\$ ${contaDaBrenna.saldo}.");
+  print("Titular: ${contaDoCainaa.titular}, saldo: R\$ ${contaDoCainaa.saldo}.");
 }
 
 class ContaCorrente {
@@ -21,16 +19,33 @@ class ContaCorrente {
   int agencia = 145;
   late int conta;
   double saldo = 20.0;
+  double chequeEspecial = -100.0;
 
-  //criando um método com retorno para a classe
-  bool saque(double valorDoSaque) {
-    if (this.saldo - valorDoSaque < -100) {
-      //this.saldo está referenciando o saldo da classe
-      //considerando que tem $100 de cheque especial
-      print('Saldo insuficiente');
+  bool verificaSaldo(double valor) {
+    if (this.saldo - valor < chequeEspecial) {
+      print('Saldo insuficiente! :c');
       return false;
     } else {
-      print('Sacando R\$ $valorDoSaque');
+      print('Movimentando R\$ $valor.');
+      this.saldo -= valor;
+      return true;
+    }
+  }
+
+  bool transferencia(double valorTransferencia, ContaCorrente contaDestino) {
+    if (!verificaSaldo(valorTransferencia)) {
+      return false;
+    } else {
+      contaDestino.deposito(valorTransferencia);
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque) {
+    if (!verificaSaldo(valorDoSaque)) {
+      return false;
+    } else {
+      print('Sacando R\$ $valorDoSaque.');
       this.saldo -= valorDoSaque;
       return true;
     }
